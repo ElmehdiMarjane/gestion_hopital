34fb3f375fa77facebcb84bfe9a5e8e42f241086
Elmehdi marjane <johndoe@example.com>
2022-04-23 02:37:14 +0000

    Initial commit

---
+++ src/main/java/gestionhopital/elmehdimarjane/gestion_hopital/GestionHopitalApplication.java
@@ -0,0 +1,13 @@
+package gestionhopital.elmehdimarjane.gestion_hopital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHopitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionHopitalApplication.class, args);
+    }
+
+}
