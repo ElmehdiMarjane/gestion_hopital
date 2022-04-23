70bd82c894b12d68db32272d9efbb76b639651e0
Elmehdi marjane <johndoe@example.com>
2022-04-23 02:43:09 +0000

    First test comit

--- src/main/java/gestionhopital/elmehdimarjane/gestion_hopital/GestionHopitalApplication.java
+++ src/main/java/gestionhopital/elmehdimarjane/gestion_hopital/GestionHopitalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionHopitalApplication {
-
+    public String Test;
     public static void main(String[] args) {
         SpringApplication.run(GestionHopitalApplication.class, args);
     }
