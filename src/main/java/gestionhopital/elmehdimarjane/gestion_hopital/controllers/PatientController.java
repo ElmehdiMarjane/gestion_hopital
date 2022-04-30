275ac0553603f3d362b01d0454ae8d380ca3bb7a
Elmehdi marjane <johndoe@example.com>
2022-04-30 01:48:08 +0000

    Pagenation

--- src/main/java/gestionhopital/elmehdimarjane/gestion_hopital/controllers/PatientController.java
+++ src/main/java/gestionhopital/elmehdimarjane/gestion_hopital/controllers/PatientController.java
@@ -5,12 +5,11 @@ import gestionhopital.elmehdimarjane.gestion_hopital.entities.Patient;
 import gestionhopital.elmehdimarjane.gestion_hopital.repositories.PatientRepository;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +20,15 @@ private PatientRepository patientRepository;
 
 
     @GetMapping("/index")
-    public String patient(Model model){
-        List<Patient> patientList=patientRepository.findAll();
-        model.addAttribute("ListPatient",patientList);
+    public String patient(Model model,
+                          @RequestParam(name = "page",defaultValue = "0") int page,
+                          @RequestParam(name = "size",defaultValue = "10")int size
+
+    ){
+        Page<Patient> patientPage=patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("ListPatient",patientPage);
+        model.addAttribute("Pages",new int[patientPage.getTotalPages()]);
+        model.addAttribute("CurrentPage",page);
         return "/patients";
 
     }
